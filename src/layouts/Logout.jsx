218a8c59a58aec969a6cc39df4cb36fab2810ce7
gokcenaysu <gokcenaysu@gmail.com>
2022-09-06 14:18:51 +0300

    adding 'login-logout' features

---
+++ src/layouts/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export default function Logout() {
+  return (
+    <div>
+        <Button variant="contained">Login</Button>
+        <Button variant="contained">Register</Button>
+    </div>
+  )
+}
